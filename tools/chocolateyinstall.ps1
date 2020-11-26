0eae78ca86054b7f37ded5b023d9dcb152f66120
Andrei Hutanu <andrei.hutanu@evalan.com>
2020-11-26 11:16:42 +0100

    updated to 1.2

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -8,8 +8,8 @@ $trimmed = [System.String]::Concat($basename, $ext)
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileFullPath  = $toolsDir
-  url           = 'https://github.com/tasmota/tasmotizer/releases/download/v1.1a/tasmotizer-1.1a.exe'
-  checksum      = 'A41C2129960AFD3C044931DB75C88D3FE9A15E8995A86A7C65961F82B7DCFDF3'
+  url           = 'https://github.com/tasmota/tasmotizer/releases/download/v.1.2/tasmotizer-1.2.exe'
+  checksum      = '445362b51bf855f62f9af7bb8362c8b27c7bc1ceb1dc88fd41a72de19b779969'
   checksumType  = 'sha256'
 }
 Get-ChocolateyWebFile @packageArgs -GetOriginalFileName
