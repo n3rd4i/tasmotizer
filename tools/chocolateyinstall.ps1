149cf7421e20a483a43e9dd45577b5e1b5d21ce1
Andrei Hutanu <andrei.hutanu@evalan.com>
2020-11-26 11:12:54 +0100

    dirty hack to fix auto-update

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -7,9 +7,9 @@ $ext = [io.path]::GetExtension($fileName)
 $trimmed = [System.String]::Concat($basename, $ext)
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  fileFullPath  = "$(Join-Path $toolsDir $trimmed)"
+  fileFullPath  = $toolsDir
   url           = 'https://github.com/tasmota/tasmotizer/releases/download/v1.1a/tasmotizer-1.1a.exe'
   checksum      = 'A41C2129960AFD3C044931DB75C88D3FE9A15E8995A86A7C65961F82B7DCFDF3'
   checksumType  = 'sha256'
 }
-Get-ChocolateyWebFile @packageArgs
+Get-ChocolateyWebFile @packageArgs -GetOriginalFileName
